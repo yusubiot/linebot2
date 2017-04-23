ec154378151128b041c8512da17de0686293ded4
yusub <kaitominato1412@gmail.com>
2017-04-23 17:17:16 +0700

    first commit

--- index.php
+++ index.php
@@ -19,8 +19,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "I don't know"
 			];
+			if($text == "Hi") {
+				 $messages = [
+				'type' => 'text',
+				'text' => "HEllo"
+			];
+}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
